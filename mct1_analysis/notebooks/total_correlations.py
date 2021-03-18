0b2989a74cb9346bfe6aed9bd9cc083a0a753577
j-berg <j-berg@users.noreply.github.com>
2021-03-18 17:47:18 -0600

    Update

--- mct1_analysis/notebooks/total_correlations.py
+++ mct1_analysis/notebooks/total_correlations.py
@@ -5,6 +5,7 @@ import scipy
 import numpy as np
 import pandas as pd
 import xpressplot as xp
+import matplotlib.pyplot as plt
 
 __path__ = os.getcwd()
 
@@ -57,3 +58,124 @@ results.to_csv(
         "data",
         "yeast_correlations_all.tsv"),
     sep='\t')
+
+results.head()
+
+"""
+count    4.558950e+07
+mean     6.583752e-03
+std      2.263918e-01
+min     -8.725383e-01
+25%     -1.422423e-01
+50%     -1.673049e-03
+75%      1.471100e-01
+max      1.000000e+00
+dtype: float64
+"""
+results.stack().map(lambda x: x[0]).describe()
+
+"""
+0.05   -0.357512
+0.95    0.398079
+dtype: float64
+
+represents 2.27 million connections
+
+
+0.025   -0.427949
+0.975    0.483644
+dtype: float64
+
+represents 1.14 million connections
+
+"""
+results.stack().map(lambda x: x[0]).quantile([.025, .975])
+
+g = results.stack().map(lambda x: x[0]).plot.hist(bins=50)
+fig = g.get_figure()
+fig.savefig(
+    os.path.join(
+        __path__,
+        "mct1_analysis",
+        "plots",
+        "yeast_correlations_all.png"
+    )
+)
+
+
+
+
+def g_decompress(
+        path,
+        file,
+        output):
+
+    g_open = open(os.path.join(path, output), "wb")
+    with gzip.open(os.path.join(path, file), "rb") as f:
+        g_data = f.read()
+    g_open.write(g_data)
+    g_open.close()
+
+g_decompress(
+    path=os.path.join(
+        __path__,
+        "mct1_analysis",
+        "data",
+        "analysis_lists"),
+    file="Saccharomyces_cerevisiae.R64-1-1.103.gtf.gz",
+    output="Saccharomyces_cerevisiae.R64-1-1.103.gtf"
+)
+
+gtf = pd.read_csv(
+    str(os.path.join(
+        __path__,
+        "mct1_analysis",
+        "data",
+        "analysis_lists",
+        "Saccharomyces_cerevisiae.R64-1-1.103.gtf")),
+    sep='\t',
+    comment='#',
+    low_memory=False,
+    header=None)
+
+# Parse out old and new names from GTF
+gtf_genes = gtf.loc[gtf[2] == 'gene']
+gtf_genes['original'] = gtf[8].str.split("gene_id \"").str[1].str.split("\"; ").str[0]
+gtf_genes['new'] = gtf[8].str.split("gene_name \"").str[1].str.split("\"; ").str[0]
+gene_dict = pd.Series(gtf_genes['new'].values,index=gtf_genes['original']).to_dict()
+for k in gene_dict.keys():
+    try:
+        if gene_dict[k] is np.nan:
+            gene_dict[k] = k
+    except:
+        gene_dict[k] = k
+
+os.remove(os.path.join(
+    __path__,
+    "mct1_analysis",
+    "data",
+    "analysis_lists",
+    "Saccharomyces_cerevisiae.R64-1-1.103.gtf")
+)
+
+
+import networkx as nx
+g = nx.Graph()
+
+for x in results.index.tolist():
+    g.add_node(
+        x,
+        {
+            'id': x,
+            'name': gene_dict[x]
+        }
+    )
+
+for x in results.index.tolist():
+    for y in results.index.tolist():
+
+        if results.at[x, y][0] >= 0.483644 or results.at[x, y][0] <= 0.483644 and results.at[x, y][1] < 0.05:
+            g.add_edge(x, y, {
+                'r': results.at[x, y][0],
+                'p': results.at[x, y][1]
+            })
